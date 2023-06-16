7c5bb938ea0a270d048fbc9ef3733609f442f0da
ifs20021-itdel <ifs20021@students.del.ac.id>
2023-06-16 21:56:25 +0700

    test

--- src/server.js
+++ src/server.js
@@ -1,50 +1,38 @@
 //import modul
-import express from 'express'
-import dotenv from 'dotenv'
-import logs from './middleware/logs.js' 
+import express from "express";
+import dotenv from "dotenv";
+import logs from "./middleware/logs.js";
 
-//import modul yang dibutuhkan dari routes
-import diseaseRoute from './route/diseaseRoute.js'
-import plantRoute from './route/plantRoute.js'
-import authRoute from './route/authRoute.js'
-import recommendRoute from './route/recommendRoute.js'
-import detectionRoute from './route/detectionRoute.js'
-import historyRoute from './route/historyRoute.js'
-
-
+//import modul yang dibutuhkan dari routes'
+import authRoute from "./route/authRoute.js";
 
 dotenv.config();
 
 const app = express();
+const diseaseRoute = express.Router();
 
-// middleware 
-app.use(express.json());  
-app.use(express.urlencoded({ extended: false })); 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(logs);
 
 // router
 app.use(authRoute);
 app.use(diseaseRoute);
-app.use(plantRoute);
-app.use(recommendRoute);
-app.use(detectionRoute);
-app.use(historyRoute);
-
 
 //handling routes yang tidak ditemukan
 app.use((req, res, next) => {
-    next(createError.NotFound('Tidak Ditemukan'))
-  });
+  next(createError.NotFound("Tidak Ditemukan"));
+});
 
 //handling error
-  app.use((err, req, res, next) => {
-    const { status = 500, message } = err;
-    res.status(status).json({ error: { status, message } });
-  });
-
+app.use((err, req, res, next) => {
+  const { status = 500, message } = err;
+  res.status(status).json({ error: { status, message } });
+});
 
 //port
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 2000;
 app.listen(`${port}`, () => {
-    console.log(`Server berjalan di port ${port}`)
-})
+  console.log(`Server berjalan di port ${port}`);
+});
